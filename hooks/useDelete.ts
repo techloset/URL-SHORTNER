6cea2c578f8d1f61ddbaeab8e4f495284d135d47
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-04-18 12:50:40 +0500

    fixed client-server side issues

--- hooks/useDelete.ts
+++ hooks/useDelete.ts
@@ -1,5 +1,3 @@
-"use client";
-
 import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
--- hooks/useEdit.ts
+++ hooks/useEdit.ts
@@ -1,10 +1,84 @@
-import { updateParams } from "@/types/types";
-import { useRouter } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
-import toast from "react-hot-toast";
+// import { updateParams } from "@/types/types";
+// import { useRouter } from "next/navigation";
+// import { useEffect, useRef, useState } from "react";
+// import toast from "react-hot-toast";
 
-export default function useEdit({ params }: { params: { id: string } }) {
+// export default function useEdit({ params }: { params: { id: string } }) {
+//   const router = useRouter();
+//   const updateBlog = async (data: updateParams) => {
+//     const res = await fetch(`http://localhost:3000/api/shortUrl/${data.id}`, {
+//       method: "PUT",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         longUrl: data.longUrl,
+//       }),
+//     });
+//     return (await res).json();
+//   };
+
+//   const getBlogById = async (id: string) => {
+//     try {
+//       const res = await fetch(`http://localhost:3000/api/shortUrl/${id}`);
+//       const data = await res.json();
+//       return data;
+//     } catch (error) {
+//       console.error("Error fetching URL:", error);
+//       throw error;
+//     }
+//   };
+
+//   const urlRef = useRef<HTMLInputElement | null>(null);
+
+//   useEffect(() => {
+//     toast.loading("Fetching URL 🚀", { id: "1" });
+//     getBlogById(params.id)
+//       .then((data) => {
+//         if (urlRef.current) {
+//           urlRef.current.value = data.post.longUrl;
+//           console.log(data.post.longUrl);
+
+//           toast.success("Fetching Completed", { id: "1" });
+//         }
+//       })
+//       .catch((err) => {
+//         toast.error("Fetching Failed", { id: "1" });
+//       });
+//   }, []);
+
+//   const handleUrl = async (e: any) => {
+//     e.preventDefault();
+
+//     if (urlRef.current) {
+//       toast.loading(" Sending Request 🚀", { id: "1" });
+//       await updateBlog({
+//         longUrl: urlRef.current?.value,
+//         id: params.id,
+//       });
+//       toast.success("Your URL is Updated Successfully", { id: "1" });
+//       router.push("/");
+//     }
+//   };
+
+//   const [loading, setLoading] = useState(false);
+
+//   return {
+//     loading,
+//     setLoading,
+//     handleUrl,
+//     urlRef,
+//   };
+// }
+"use client";
+import { useState, useEffect, useRef } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/router";
+import { updateParams } from "@/types/types";
+
+function useEditForm(params: { id: string }) {
   const router = useRouter();
+
   const updateBlog = async (data: updateParams) => {
     const res = await fetch(`http://localhost:3000/api/shortUrl/${data.id}`, {
       method: "PUT",
@@ -30,6 +104,7 @@ export default function useEdit({ params }: { params: { id: string } }) {
   };
 
   const urlRef = useRef<HTMLInputElement | null>(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     toast.loading("Fetching URL 🚀", { id: "1" });
@@ -37,8 +112,6 @@ export default function useEdit({ params }: { params: { id: string } }) {
       .then((data) => {
         if (urlRef.current) {
           urlRef.current.value = data.post.longUrl;
-          console.log(data.post.longUrl);
-
           toast.success("Fetching Completed", { id: "1" });
         }
       })
@@ -61,12 +134,7 @@ export default function useEdit({ params }: { params: { id: string } }) {
     }
   };
 
-  const [loading, setLoading] = useState(false);
-
-  return {
-    loading,
-    setLoading,
-    handleUrl,
-    urlRef,
-  };
+  return { urlRef, handleUrl, loading };
 }
+
+export default useEditForm;
